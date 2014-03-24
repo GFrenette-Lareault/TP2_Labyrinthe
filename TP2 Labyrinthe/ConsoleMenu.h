56650bc929796cb115728819e7f5255aa2ae551c
Jean-David <jeandavidmoisan@gmail.com>
2014-03-24 15:45:35 -0400

    Work Continues.

--- TP2 Labyrinthe/ConsoleMenu.h
+++ TP2 Labyrinthe/ConsoleMenu.h
@@ -7,7 +7,6 @@ class ConsoleMenu
 {
 public:
 	void Run();
-
 	ConsoleMenu();
 	~ConsoleMenu();
 
---
+++ TP2 Labyrinthe/Square.cpp
@@ -0,0 +1,11 @@
+struct Square 
+{
+	int x;
+	int y;
+	char value;
+	bool visited;
+	Square* upperSquare;
+	Square* rightSquare;
+	Square* leftSquare;
+	Square* lowerSquare;
+};
